6fb7519a442d0e6b21e56f7c3bea7d474050dcf9
ukmengse@gmail.com <ukmengse@gmail.com>
2025-07-23 15:35:46 +0800

    feat: 预览不同类型的噪声采样

--- Packages/com.rs.scene/Editor/SamplerTestWindow.cs
+++ Packages/com.rs.scene/Editor/SamplerTestWindow.cs
@@ -27,6 +27,11 @@ namespace RS.Scene
         // 预览视角 0 = side, 1 = top
         private byte m_previewMode = 0;
         private readonly string[] m_previewModeStrs = { "Side", "Top" };
+
+        // 预览噪声 0 = Continentalness, 1 = Erosion, 2 = Peak & Valleys, 3 = Temperature, 4 = Humidity
+        private byte m_noiseMode = 0;
+        private readonly string[] m_noiseModeStrs =
+            { "Continentalness", "Erosion", "Peak & Valleys", "Temperature", "Humidity" };
         
         private Texture2D m_texture;
 
@@ -76,6 +81,12 @@ namespace RS.Scene
             EditorGUILayout.LabelField("预览视角", labelStyle, GUILayout.Width(60));
             m_previewMode = (byte)GUILayout.Toolbar(m_previewMode, m_previewModeStrs);
             EditorGUILayout.EndHorizontal();
+            
+            // 选择生成的噪声
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("预览噪声", labelStyle, GUILayout.Width(60));
+            m_noiseMode = (byte)GUILayout.Toolbar(m_noiseMode, m_noiseModeStrs);
+            EditorGUILayout.EndHorizontal();
 
             // 采样并生成纹理
             if (GUILayout.Button("Generate", buttonStyle))
@@ -85,13 +96,17 @@ namespace RS.Scene
                 // TODO: Use Config
             
                 // Temperature
-                // var amplitudes = new float[] { 1.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f };
-                // var firstOctave = -10;
-            
-                // Continentalness
-                // var amplitudes = new float[] { 1.0f, 1.0f, 2.0f, 2.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-                // var firstOctave = -9;
-            
+                var tempAmps = new float[] { 1.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f };
+                var tempFirstOctave = -10;
+                var temperatureNoise = new RsNoise(rng.NextUInt64(), tempAmps, tempFirstOctave);
+                var temperatureSampler = new RsSampler(temperatureNoise);
+                
+                // Humidity
+                var humidityAmps = new float[] { 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+                var humidityFirstOctave = -8;
+                var humidityNoise = new RsNoise(rng.NextUInt64(), humidityAmps, humidityFirstOctave);
+                var humiditySampler = new RsSampler(humidityNoise);
+                
                 // offset
                 var amplitudes = new float[] { 1.0f, 1.0f, 1.0f, 0.0f };
                 var firstOctave = -3;
@@ -111,9 +126,9 @@ namespace RS.Scene
                 var offsetNoise2 = new RsNoise(rng.NextUInt64(), amplitudes, firstOctave);
                 var shiftZSampler = new FlatCacheSampler(new Cache2DSampler(new ShiftBSampler(offsetNoise2)));
 
+                // ridges
                 var ridgesSampler = new FlatCacheSampler(new ShiftedNoiseSampler(ridgesNoise, shiftXSampler,
                     new ConstantSampler(0.0f), shiftZSampler, 0.25f, 0.0f));
-
                 var ridgesFoldedSampler = new MulSampler(new ConstantSampler(-3.0f),
                     new AddSampler(new ConstantSampler(-0.33333f),
                         new AbsSampler(new AddSampler(new ConstantSampler(-0.66666f), new AbsSampler(ridgesSampler)))));
@@ -122,13 +137,48 @@ namespace RS.Scene
                 var erosionAmps = new float[] { 1.0f, 1.0f, 0.0f, 1.0f, 1.0f };
                 var eroFirstOctave = -9;
                 var erosionNoise = new RsNoise(rng.NextUInt64(), erosionAmps, eroFirstOctave);
-
                 var erosionSampler = new FlatCacheSampler(new ShiftedNoiseSampler(erosionNoise, shiftXSampler,
                     new ConstantSampler(0.0f), shiftZSampler, 0.25f, 0.0f));
                 
+                // Continentalness
+                var continentAmps = new float[] { 1.0f, 1.0f, 2.0f, 2.0f, 2.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+                var continentFirstOctave = -9;
+                var continentNoise = new RsNoise(rng.NextUInt64(), continentAmps, continentFirstOctave);
+                var continentsSampler = new FlatCacheSampler(new ShiftedNoiseSampler(continentNoise, shiftXSampler,
+                    new ConstantSampler(0.0f), shiftZSampler, 0.25f, 0.0f));
                 
-                m_texture = Sample(erosionSampler);
+                switch (m_noiseMode)
+                {
+                    case 0:
+                    {
+                        m_texture = Sample(continentsSampler);
+                        break;
+                    }
+                    case 1:
+                    {
+                        m_texture = Sample(erosionSampler);
+                        break;
+                    }
+                    case 2:
+                    {
+                        m_texture = Sample(ridgesFoldedSampler);
+                        break;
+                    }
+                    case 3:
+                    {
+                        m_texture = Sample(temperatureSampler);
+                        break;
+                    }
+                    case 4:
+                    {
+                        m_texture = Sample(humiditySampler);
+                        break;
+                    }
+                }
+                // m_texture = Sample(continentsSampler);
             }
+            
+            
             
             // 显示Texture2D
             if (m_texture != null)
