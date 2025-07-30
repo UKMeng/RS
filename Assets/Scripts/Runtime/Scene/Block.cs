7fe15beaa23f1b0fca6fef9a7d655b27b0600c70
ukmengse@gmail.com <ukmengse@gmail.com>
2025-07-30 15:51:34 +0800

    feat: 关闭mipmap

--- Assets/Scripts/Runtime/Scene/Block.cs
+++ Assets/Scripts/Runtime/Scene/Block.cs
@@ -10,6 +10,7 @@ namespace RS.Scene
         Air,
         Stone,
         Dirt,
+        Sand,
         Water,
     }
     
@@ -29,9 +30,11 @@ namespace RS.Scene
         {
             uvTable = new Vector2[Enum.GetValues(typeof(BlockType)).Length][];
 
-            uvTable[(int)BlockType.Water] = CalUVs(0, 0);
-            uvTable[(int)BlockType.Stone] = CalUVs(0, 1);
-            uvTable[(int)BlockType.Dirt] = CalUVs(0, 2);
+            uvTable[(int)BlockType.Stone] = CalUVs(0, 0);
+            uvTable[(int)BlockType.Water] = CalUVs(1, 0);
+            
+            uvTable[(int)BlockType.Dirt] = CalUVs(0, 1);
+            uvTable[(int)BlockType.Sand] = CalUVs(1, 1);
 
             uvTableArray = new NativeArray<Vector2>(uvTable.Length * 4, Allocator.Persistent);
 
@@ -77,10 +80,10 @@ namespace RS.Scene
 
         private static Vector2[] CalUVs(int x, int y)
         {
-            var margin = 10;
-            var width = 1044;
-            var height = 3132;
-            var textureSize = 1024;
+            var margin = 8;
+            var width = 4128;
+            var height = 4128;
+            var textureSize = 2048;
             
             var uvs = new Vector2[4];
             uvs[0] = new Vector2((float)(x * textureSize + (2 * x + 1) * margin) / width, (float)(y * textureSize + (2 * y + 1) * margin) / height);
