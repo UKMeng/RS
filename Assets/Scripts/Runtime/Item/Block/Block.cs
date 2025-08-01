0236b26ee38f8f7d6e51c5ad155e4682f25a289d
ukmengse@gmail.com <ukmengse@gmail.com>
2025-08-01 21:14:19 +0800

    art: 测试高清美术素材

--- Assets/Scripts/Runtime/Item/Block/Block.cs
+++ Assets/Scripts/Runtime/Item/Block/Block.cs
@@ -32,10 +32,8 @@ namespace RS.Item
             uvTable = new Vector2[Enum.GetValues(typeof(BlockType)).Length][];
 
             uvTable[(int)BlockType.Stone] = CalUVs(0, 0);
-            uvTable[(int)BlockType.Water] = CalUVs(1, 0);
-            
-            uvTable[(int)BlockType.Dirt] = CalUVs(0, 1);
-            uvTable[(int)BlockType.Sand] = CalUVs(1, 1);
+            uvTable[(int)BlockType.Dirt] = CalUVs(1, 0);
+            uvTable[(int)BlockType.Sand] = CalUVs(0, 1);
 
             uvTableArray = new NativeArray<Vector2>(uvTable.Length * 4, Allocator.Persistent);
 
@@ -61,10 +59,10 @@ namespace RS.Item
 
         private static Vector2[] CalUVs(int x, int y)
         {
-            var margin = 8;
-            var width = 4128;
-            var height = 4128;
-            var textureSize = 2048;
+            var margin = 0;
+            var width = 2048;
+            var height = 2048;
+            var textureSize = 1024;
             
             var uvs = new Vector2[4];
             uvs[0] = new Vector2((float)(x * textureSize + (2 * x + 1) * margin) / width, (float)(y * textureSize + (2 * y + 1) * margin) / height);
