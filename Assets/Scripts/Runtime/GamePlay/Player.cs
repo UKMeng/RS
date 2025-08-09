53d345eb67093db045346a422fbbe1b08520d2a0
UKMeng <ukmengse@gmail.com>
2025-08-09 14:17:56 +0800

    bugfix: 修正漂浮状态检测错误，更改地图键位

--- Assets/Scripts/Runtime/GamePlay/Player.cs
+++ Assets/Scripts/Runtime/GamePlay/Player.cs
@@ -134,7 +134,7 @@ namespace RS.GamePlay
 
         public bool Floating
         {
-            get { return m_isInWater && m_transform.position.y < 62.5; }
+            get { return m_isInWater && m_transform.position.y < 62.49; }
         }
 
         public bool Sprint
