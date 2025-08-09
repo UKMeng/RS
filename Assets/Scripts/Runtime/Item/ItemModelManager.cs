d2635f2a93275cfc534980397ad2b6b2e78b82be
UKMeng <ukmengse@gmail.com>
2025-08-10 01:45:13 +0800

    feat: 道具模型

---
+++ Assets/Scripts/Runtime/Item/ItemModelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RS.Item
+{
+    public class ItemModelManager : MonoBehaviour
+    {
+        [SerializeField] private Transform m_rightHand;
+        [SerializeField] private List<GameObject> m_itemPrefabs;
+
+        public void Start()
+        {
+            Instantiate(m_itemPrefabs[2], m_rightHand);
+        }
+    }
+}
